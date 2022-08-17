9be82d1c5fa122fead41f8f45d63582f48c05f7a
Valeriya <valeriyalukovkina@gmail.com>
2022-08-17 15:05:16 +0800

    ready

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import {useProducts} from './hooks/products'
 function App() {
   const {products} = useProducts()
 
+
+
   return (
     <>
       <div className='wrp'>
@@ -13,55 +15,55 @@ function App() {
         <div className='shisha group'>
           <p className='shisha_title group_title'>Кальяны</p>
           <div className='shisha_name group_name'>
-            {products.map(product => product.type === 'Кальяны' && <Product product={product}/>)}
+            {products.map(product => product.type === 'Кальяны' && <Product product={product} key={product.id}/>)}
           </div>
         </div>
         <div className='coffeetea group'>
           <p className='coffeetea_title group_title'>Кофе и чай</p>
           <div className='coffeetea_name group_name'>
-            {products.map(product => product.type === 'Кофе и чай' && <Product product={product}/>)}
+            {products.map(product => product.type === 'Кофе и чай' && <Product product={product} key={product.id}/>)}
           </div>
         </div>
         <div className='pizza group'>
           <h3 className='pizza_title group_title'>Пироги и пиццы</h3>
           <div className='pizza_name group_name'>
-            {products.map(product => product.type === 'Пироги и пиццы' && <Product product={product}/>)}
+            {products.map(product => product.type === 'Пироги и пиццы' && <Product product={product} key={product.id}/>)}
           </div>
         </div>
         <div className='salad group'>
           <p className='salad_title group_title'>Салаты</p>
           <div className='salad_name group_name'>
-            {products.map(product => product.type === 'Салаты' && <Product product={product}/>)}
+            {products.map(product => product.type === 'Салаты' && <Product product={product} key={product.id}/>)}
           </div>
         </div>
         <div className='grill group'>
           <p className='grill_title group_title'>Блюда на углях</p>
           <div className='grill_name group_name'>
-            {products.map(product => product.type === 'Блюда на углях' && <Product product={product}/>)}
+            {products.map(product => product.type === 'Блюда на углях' && <Product product={product} key={product.id}/>)}
           </div>
         </div>
         <div className='garnish group'>
           <h3 className='garnish_title group_title'>Гарниры</h3>
           <div className='garnish_name group_name'>
-            {products.map(product => product.type === 'Гарниры' && <Product product={product}/>)}
+            {products.map(product => product.type === 'Гарниры' && <Product product={product} key={product.id}/>)}
           </div>
         </div>
         <div className='lemonade group'>
           <h3 className='lemonade_title group_title'>Лимонады</h3>
           <div className='lemonade_name group_name'>
-            {products.map(product => product.type === 'Лимонады' && <Product product={product}/>)}
+            {products.map(product => product.type === 'Лимонады' && <Product product={product} key={product.id}/>)}
           </div>
         </div>
         <div className='icecream group'>
           <p className='icecream_title group_title'>Мороженное</p>
           <div className='icecream_name group_name'>
-            {products.map(product => product.type === 'Мороженное' && <Product product={product}/>)}
+            {products.map(product => product.type === 'Мороженное' && <Product product={product} key={product.id}/>)}
           </div>
         </div>
         <div className='milkshake group'>
           <p className='milkshake_title group_title'>Милкшейки</p>
           <div className='milkshake_name group_name'>
-            {products.map(product => product.type === 'milkshake' && <Product product={product}/>)}
+            {products.map(product => product.type === 'milkshake' && <Product product={product} key={product.id}/>)}
           </div>
         </div>
       </div>
--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,15 +1,15 @@
 import React, {useState} from "react"
-// import { products } from "../data/products"
 import { IProduct } from "../models"
 import {useProducts} from '../hooks/products'
 
 interface ProductProps {
     product: IProduct
 }
+
 export function Product (props: ProductProps) {
-    // const {products} = useProducts()
+    const {products} = useProducts()
     const [details, setDetails] = useState(false)
-    const [showBtn, setShowBtn] = useState(true)
+    // const [showBtn, setShowBtn] = useState(true)
     const root = document.querySelector('#root') as HTMLElement
     let body = document.querySelector('body') as HTMLElement
     function addDescription () {
@@ -37,13 +37,13 @@ export function Product (props: ProductProps) {
         <>
             <div className="product">
                 <span className="product_name">{props.product.name}</span>
-                <span className="product_price">{props.product.price} &#8381;</span>
-                {showBtn && <button 
+                <span className="product_price">{props.product.price} &#8381;</span>\
+                {props.product.description !== '' && <button 
                     className="product_btn"
                     onClick={addDescription}
                 >
                     Подробнее...
-                </button> }
+                </button>}
             </div>
             {details && <div className="product_description">
                 <button 
@@ -51,7 +51,8 @@ export function Product (props: ProductProps) {
                     onClick={closeDescription}
                     >
                 </button>
-                <img className="product_description_img" src={'http://yama-new.ru/images/'+props.product.imageId} alt="image" />
+                {props.product.imageId && <img className="product_description_img" src={'http://yama-new.ru/images/'+props.product.imageId} alt="image" />}
+                {/* <img className="product_description_img" src={'http://yama-new.ru/images/'+props.product.imageId} alt="image" /> */}
                 <span className="product_description_txt">{props.product.description}</span>
             </div>}
         </>
